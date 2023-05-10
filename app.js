8abbe84e443360e0ddc5450f2f941c72b3c93253
MarcoPerta <perta.marco@gmail.com>
2023-05-10 10:03:43 +0200

    switch sections

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controls');
 const sectBtn = document.querySelectorAll('.control');
-const allSection = document.querySelector('main-content');
+const allSection = document.querySelector('.main-content');
+
 
 function PageTransition(){
     //btn classe attiva
@@ -12,6 +13,26 @@ function PageTransition(){
             this.className += ' active-btn';
         })
     }
+
+    //section classi attive 
+    allSection.addEventListener('click', (e) =>{
+        const id = e.target.dataset.id;
+        if(id){
+
+            sectBtns.forEach((btn) =>{
+                btn.classList.remove('active');
+            })
+            e.target.classList.add('active')
+
+            sections.forEach((section) =>{
+                section.classList.remove('active');
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+
+        }
+    })
 }
 
 PageTransition();
