a1873adeab77accdcd9b90f1e4354558316a9e3c
MarcoPerta <perta.marco@gmail.com>
2023-05-12 11:12:25 +0200

    light mode

--- app.js
+++ app.js
@@ -75,6 +75,14 @@ function PageTransition(){
   
   contactForm.addEventListener('submit', sendEmail);
 
+
+  //toggle theme
+  const themeBtn = document.querySelector('.theme-btn');
+  themeBtn.addEventListener('click',() =>{
+    let element = document.body;
+    element.classList.toggle('light-mode');
+  })
+
 }
 
 PageTransition();
